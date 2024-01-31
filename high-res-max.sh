cd0a54d4c1088a8177ef486b936a08e282ce4c58
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2024-01-31 12:04:09 +0000

    Get height with code

--- high-res-max.sh
+++ high-res-max.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Get screen dimensions
+screen_dimensions=$(xdpyinfo | awk '/dimensions:/ {print $2}')
+
+# Extract height from screen dimensions
+height=$(echo $screen_dimensions | cut -d 'x' -f 2)
+
 # Set your predefined width, height, and space at the bottom
 WIDTH=4000
 SPACE_HEIGHT=280  # Assuming 5cm (5*25.4) converted to pixels
-FULL_HEIGHT=2400
-
+FULL_HEIGHT=$(echo $screen_dimensions | cut -d 'x' -f 2)
 
 # Calculate the window height
 HEIGHT=$((FULL_HEIGHT - SPACE_HEIGHT))
