aeece1ecb598967cdc74b6ebf6892c0f75e254cd
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2024-01-31 12:26:55 +0000

    If it's chrome then leave less of a bit

--- high-res-max.sh
+++ high-res-max.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Get the active window name
+window_name=$(xdotool getwindowname $(xdotool getactivewindow))
+
+# Check if the window name contains "Visual Code"
+if [[ "$window_name" == *"Chrome"* ]]; then
+    # If "Visual Code" is in the window name, set a certain percentage
+    PERCENTAGE=5
+else
+    # If "Visual Code" is not in the window name, set a different percentage
+    PERCENTAGE=11
+fi
+
 # Get screen dimensions
 screen_dimensions=$(xdpyinfo | awk '/dimensions:/ {print $2}')
 
@@ -8,8 +20,9 @@ height=$(echo $screen_dimensions | cut -d 'x' -f 2)
 
 # Set your predefined width, height, and space at the bottom
 WIDTH=4000
-SPACE_HEIGHT=280  # Assuming 5cm (5*25.4) converted to pixels
 FULL_HEIGHT=$(echo $screen_dimensions | cut -d 'x' -f 2)
+# SPACE_HEIGHT=280  # Assuming 5cm (5*25.4) converted to pixels
+SPACE_HEIGHT=$(echo "$FULL_HEIGHT * $PERCENTAGE / 100" | bc)
 
 # Calculate the window height
 HEIGHT=$((FULL_HEIGHT - SPACE_HEIGHT))
